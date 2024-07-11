65213dd452b2672e44b7d390640590ae038144c4
ayanjamil <ayanjamil00@gmail.com>
2024-07-11 15:59:52 +0530

    feat: rough landing page draft 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "PSOC BIT MESRA",
+  description: "TPhotographic Society of BIT Mesra",
 };
 
 export default function RootLayout({
